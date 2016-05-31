15b9480ea3276efa32da655435c3cf37e2014915
c/c++ <meiyou@alphathink.org>
2016-05-31 20:33:09 +0800

    Add files via upload

---
+++ 41.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d;
+	cin>>a>>b>>c;
+	
+	if(a > b)
+	{
+		d = a;
+		a = b;
+		b = d;
+	}
+	if(a > c)
+	{
+		d = a;
+		a = c;
+		c = d;
+	}
+	if(b > c)
+	{
+		d = b;
+		b = c;
+		c = d;
+	}
+	cout<<a<<" "<<b<<" "<<c<<" ";
+}
